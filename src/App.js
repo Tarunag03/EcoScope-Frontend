39a15aaafc23e3beaad395f25c55154f707d7fcb
Tarunagr03 <Tarunagr03@gmail.com>
2024-10-06 13:23:59 +0530

    landingpage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/Landingpage';
-import LoginPage from './pages/Login' // Your login/signup component
+import LandingPage from './pages/Landingpage'; // Make sure this matches the filename
+import LoginPage from './pages/LoginSignup';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/LandingPage" element={<LandingPage />} />
-        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
-
   );
 }
 
